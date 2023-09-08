c5d6f1461d96f0a28ad129a942f7faba5b779798
Federico Duek <fedebrilduek@gmail.com>
2023-09-07 23:51:35 -0300

    tarea3

--- juego.js
+++ juego.js
@@ -3,6 +3,16 @@ function generarNumeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Declaración de variables y objetos necesarios
+const intentosElement = document.getElementById("intentos");
+const resultadoElement = document.getElementById("resultado");
+const intentoUsuarioElement = document.getElementById("intentoUsuario");
+const realizarIntentoButton = document.getElementById("realizarIntento");
+
+let intentos = 0;
+const maxIntentos = 10;
+let numeroSecreto = generarNumeroAleatorio(1, 100);
+
 // Función para verificar si el número ingresado es válido
 function validarNumero(numero) {
   return !isNaN(numero) && Number.isInteger(numero) && numero >= 1 && numero <= 100;
@@ -21,101 +31,44 @@ function verificarIntento(intentoUsuario, numeroSecreto) {
 
 // Función principal del juego
 function jugarAdivinanza() {
-  const numeroSecreto = generarNumeroAleatorio(1, 100);
-  let intentos = 0;
-  const maxIntentos = 10;
+  intentos++;
+  const intentoUsuario = parseInt(intentoUsuarioElement.value);
 
-  const hacerIntento = () => {
-    const intentoUsuarioStr = prompt('Adivina el número secreto (entre 1 y 100): ');
-    const intentoUsuario = parseInt(intentoUsuarioStr);
+  if (!validarNumero(intentoUsuario)) {
+    resultadoElement.textContent = 'Por favor, ingresa un número válido entre 1 y 100.';
+    return;
+  }
 
-    if (!validarNumero(intentoUsuario)) {
-      console.log('Por favor, ingresa un número válido entre 1 y 100.');
-      hacerIntento();
-      return;
-    }
+  const resultado = verificarIntento(intentoUsuario, numeroSecreto);
+  intentosElement.textContent = intentos;
 
-    intentos++;
+  if (resultado === 'acierto') {
+    resultadoElement.textContent = `¡Felicitaciones! Adivinaste el número secreto ${numeroSecreto} en ${intentos} intentos.`;
+  } else if (resultado === 'mayor') {
+    resultadoElement.textContent = 'El número secreto es mayor. ¡Sigue intentando!';
+  } else {
+    resultadoElement.textContent = 'El número secreto es menor. ¡Sigue intentando!';
+  }
 
-    const resultado = verificarIntento(intentoUsuario, numeroSecreto);
+  if (intentos >= maxIntentos || resultado === 'acierto') {
+    realizarIntentoButton.disabled = true;
+  }
 
-    if (resultado === 'acierto') {
-      console.log(`¡Felicitaciones! Adivinaste el número secreto ${numeroSecreto} en ${intentos} intentos.`);
-    } else if (resultado === 'mayor') {
-      console.log('El número secreto es mayor. ¡Sigue intentando!');
-      if (intentos < maxIntentos) {
-        hacerIntento();
-      } else {
-        console.log(`Agotaste los ${maxIntentos} intentos. El número secreto era ${numeroSecreto}.`);
-      }
-    } else {
-      console.log('El número secreto es menor. ¡Sigue intentando!');
-      if (intentos < maxIntentos) {
-        hacerIntento();
-      } else {
-        console.log(`Agotaste los ${maxIntentos} intentos. El número secreto era ${numeroSecreto}.`);
-      }
-    }
-  };
-
-  hacerIntento();
+  // Almacenar el resultado en el localStorage utilizando JSON
+  const historial = JSON.parse(localStorage.getItem('historial')) || [];
+  historial.push({ intentoUsuario, resultado });
+  localStorage.setItem('historial', JSON.stringify(historial));
 }
 
 // Llamada a la función para iniciar el juego
-jugarAdivinanza();
+realizarIntentoButton.addEventListener('click', jugarAdivinanza);
 
-// Objeto que representa a una persona
-const persona = {
-  nombre: "Juan",
-  edad: 30,
-  ocupacion: "Desarrollador"
-};
-
-console.log("Información de la persona:");
-console.log("Nombre:", persona.nombre);
-console.log("Edad:", persona.edad);
-console.log("Ocupación:", persona.ocupacion);
-
-// Declaración de variables y objetos necesarios
-const numero1 = 10;
-const numero2 = 20;
-const cadena1 = "Hola, ";
-const cadena2 = "¿cómo estás?";
-const arrayNumeros = [5, 10, 15, 20, 25];
-
-// Funciones esenciales del proceso a simular
-function suma(a, b) {
-  return a + b;
-}
-
-function resta(a, b) {
-  return a - b;
-}
-
-function concatenar(str1, str2) {
-  return str1 + str2;
-}
-
-function division(a, b) {
-  if (b !== 0) {
-    return a / b;
-  } else {
-    return "No es posible dividir por cero.";
+// Recuperar datos almacenados en el localStorage al cargar la página
+window.addEventListener('load', function () {
+  const historial = JSON.parse(localStorage.getItem('historial'));
+  if (historial) {
+    historial.forEach(item => {
+      console.log(`Intento: ${item.intentoUsuario}, Resultado: ${item.resultado}`);
+    });
   }
-}
-
-function porcentaje(total, porcentaje) {
-  return (total * porcentaje) / 100;
-}
-
-// Uso de las funciones
-console.log("Suma:", suma(numero1, numero2));
-console.log("Resta:", resta(numero1, numero2));
-console.log("Concatenación:", concatenar(cadena1, cadena2));
-console.log("División:", division(numero1, numero2));
-console.log("Porcentaje:", porcentaje(numero1, 20));
-
-// Arrays y métodos de búsqueda y filtrado
-console.log("Array:", arrayNumeros);
-console.log("Índice de 15:", arrayNumeros.indexOf(15));
-console.log("Elementos mayores que 10:", arrayNumeros.filter(num => num > 10));
+});

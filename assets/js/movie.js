3e108128441b8787288446dadf9630f8a36865dc
Federico Duek <fedebrilduek@gmail.com>
2023-10-12 10:25:13 -0300

    entrega final

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,80 +1,83 @@
-// Función para obtener una película aleatoria desde la API de TMDb
 function obtenerPeliculaAleatoria() {
-    const apiKey = 'eeae407401640aef19a5c29284e82077'; 
-    const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
-  
-    return axios.get(apiUrl)
+  const apiKey = 'eeae407401640aef19a5c29284e82077';
+  const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+
+  return fetch(apiUrl)
       .then(response => {
-        const peliculas = response.data.results;
-        const peliculaAleatoria = peliculas[Math.floor(Math.random() * peliculas.length)];
-        return peliculaAleatoria;
+          if (response.ok) {
+              return response.json();
+          }
+          throw new Error('Error al cargar la película aleatoria.');
+      })
+      .then(data => {
+          const peliculas = data.results;
+          const peliculaAleatoria = peliculas[Math.floor(Math.random() * peliculas.length)];
+          return peliculaAleatoria;
       })
       .catch(error => {
-        console.error('Error al cargar la película aleatoria:', error);
-        throw error; // Propaga el error para que se maneje fuera de la función
+          console.error('Error al cargar la película aleatoria:', error);
+          throw error;
       });
+}
+
+function mostrarPelicula(pelicula) {
+  const peliculaContainer = document.getElementById('pelicula-container');
+  peliculaContainer.innerHTML = '';
+
+  const tituloElement = document.createElement('h2');
+  tituloElement.textContent = `Adivina la Película (${pelicula.release_date.split('-')[0]})`;
+  peliculaContainer.appendChild(tituloElement);
+
+  const imagenElement = document.createElement('img');
+  imagenElement.src = `https://image.tmdb.org/t/p/w500${pelicula.poster_path}`;
+  imagenElement.alt = `Poster de ${pelicula.title}`;
+  peliculaContainer.appendChild(imagenElement);
+
+  const peliculaTextoElement = document.createElement('p');
+  peliculaTextoElement.textContent = pelicula.title.replace(/[a-zA-Z]/g, '_');
+  peliculaTextoElement.setAttribute('id', 'pelicula-texto');
+  peliculaContainer.appendChild(peliculaTextoElement);
+
+  const inputRespuesta = document.createElement('input');
+  inputRespuesta.setAttribute('type', 'text');
+  inputRespuesta.setAttribute('id', 'input-respuesta');
+  peliculaContainer.appendChild(inputRespuesta);
+
+  const botonComprobar = document.createElement('button');
+  botonComprobar.textContent = 'Comprobar Respuesta';
+  peliculaContainer.appendChild(botonComprobar);
+
+  botonComprobar.addEventListener('click', () => comprobarRespuesta(pelicula.title));
+}
+
+function comprobarRespuesta(respuestaCorrecta) {
+  const inputRespuesta = document.getElementById('input-respuesta');
+  const respuestaUsuario = inputRespuesta.value.toLowerCase();
+  const tituloPelicula = respuestaCorrecta.toLowerCase();
+
+  if (respuestaUsuario === tituloPelicula) {
+      mostrarResultado('¡Respuesta correcta!');
+  } else {
+      mostrarResultado('Respuesta incorrecta. Intenta de nuevo.');
   }
-  
-  // Función para iniciar el juego
-  function iniciarJuego() {
-    obtenerPeliculaAleatoria()
+}
+
+function mostrarResultado(resultado) {
+  const peliculaTextoElement = document.getElementById('pelicula-texto');
+  peliculaTextoElement.textContent = resultado;
+
+  const inputRespuesta = document.getElementById('input-respuesta');
+  inputRespuesta.value = '';
+
+  setTimeout(iniciarJuego, 1000); 
+}
+
+function iniciarJuego() {
+  obtenerPeliculaAleatoria()
       .then(pelicula => {
-        const tituloPelicula = pelicula.title;
-        const espaciosEnBlanco = tituloPelicula.replace(/[a-zA-Z]/g, '_');
-        mostrarPelicula(espaciosEnBlanco, tituloPelicula);
+          mostrarPelicula(pelicula);
       })
       .catch(error => console.error(error));
-  }
-  
-  // Función para mostrar la película en la interfaz
-  function mostrarPelicula(peliculaOculta, tituloPelicula) {
-    const peliculaContainer = document.getElementById('pelicula-container');
-    peliculaContainer.innerHTML = '';
-  
-    const tituloElement = document.createElement('h2');
-    tituloElement.textContent = 'Adivina la Película';
-    peliculaContainer.appendChild(tituloElement);
-  
-    const peliculaTextoElement = document.createElement('p');
-    peliculaTextoElement.textContent = peliculaOculta;
-    peliculaTextoElement.setAttribute('id', 'pelicula-texto');
-    peliculaContainer.appendChild(peliculaTextoElement);
-  
-    const inputRespuesta = document.createElement('input');
-    inputRespuesta.setAttribute('type', 'text');
-    inputRespuesta.setAttribute('id', 'input-respuesta');
-    peliculaContainer.appendChild(inputRespuesta);
-  
-    const botonComprobar = document.createElement('button');
-    botonComprobar.textContent = 'Comprobar Respuesta';
-    peliculaContainer.appendChild(botonComprobar);
-  
-    botonComprobar.addEventListener('click', () => comprobarRespuesta(tituloPelicula));
-  }
-  
-  // Función para comprobar si la respuesta es correcta
-  function comprobarRespuesta(respuestaCorrecta) {
-    const inputRespuesta = document.getElementById('input-respuesta');
-    const respuestaUsuario = inputRespuesta.value.toLowerCase();
-    const tituloPelicula = respuestaCorrecta.toLowerCase();
-  
-    if (respuestaUsuario === tituloPelicula) {
-      mostrarResultado('¡Respuesta correcta!');
-    } else {
-      mostrarResultado('Respuesta incorrecta. Intenta de nuevo.');
-    }
-  }
-  
-  // Función para mostrar el resultado en la interfaz
-  function mostrarResultado(resultado) {
-    const peliculaTextoElement = document.getElementById('pelicula-texto');
-    peliculaTextoElement.textContent = resultado;
-  
-    const inputRespuesta = document.getElementById('input-respuesta');
-    inputRespuesta.value = '';
-  
-    setTimeout(iniciarJuego, 2000); // Espera 2 segundos antes de mostrar la siguiente película
-  }
-  
-  // Iniciar el juego al cargar la página
-  window.addEventListener('load', iniciarJuego);  
+}
+
+window.addEventListener('load', iniciarJuego);
--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -1,51 +1,84 @@
-// Función para obtener preguntas de trivia desde una API externa
+// preguntas de trivia 
 function obtenerPreguntaTrivia() {
-    return axios.get('https://opentdb.com/api.php?amount=1&type=multiple')
-      .then(response => response.data.results[0])
-      .catch(error => {
-        console.error('Error al cargar la pregunta de trivia:', error);
-        throw error; // Propaga el error para que se maneje fuera de la función
-      });
-  }
-  
-  // Función para mostrar una pregunta en la interfaz
-  function mostrarPregunta(pregunta) {
-    const preguntaTexto = document.getElementById('pregunta');
-    preguntaTexto.textContent = pregunta.question;
-  
-    const respuestasContainer = document.getElementById('respuestas');
-    respuestasContainer.innerHTML = '';
-  
-    const respuestas = [...pregunta.incorrect_answers, pregunta.correct_answer];
-    respuestas.sort(() => Math.random() - 0.5); // Baraja las respuestas
-  
-    respuestas.forEach(respuesta => {
-      const botonRespuesta = document.createElement('button');
-      botonRespuesta.textContent = respuesta;
-      botonRespuesta.addEventListener('click', () => comprobarRespuesta(respuesta, pregunta.correct_answer));
-      respuestasContainer.appendChild(botonRespuesta);
-    });
-  }
-  
-  // Función para comprobar si la respuesta es correcta
-  function comprobarRespuesta(respuesta, respuestaCorrecta) {
-    const resultado = document.getElementById('resultado');
-    if (respuesta === respuestaCorrecta) {
-      resultado.textContent = '¡Respuesta correcta!';
-    } else {
-      resultado.textContent = 'Respuesta incorrecta. La respuesta correcta era: ' + respuestaCorrecta;
+  // preguntas 
+  const preguntas = [
+    {
+      question: "¿Cuál es la capital de Francia?",
+      incorrect_answers: ["Londres", "Madrid", "Berlín"],
+      correct_answer: "París"
+    },
+    {
+      question: "¿Cuál es el planeta más grande del sistema solar?",
+      incorrect_answers: ["Tierra", "Marte", "Venus"],
+      correct_answer: "Júpiter"
+    },
+    {
+      question: "¿Cual es la seleccion con mas mundiales?",
+      incorrect_answers: ["Alemania", "Italia", "Francia"],
+      correct_answer: "Brasil"
+    },
+    {
+      question: "¿Quien es el jugador con mas balones de oro?",
+      incorrect_answers: ["Maradona", "Henry", "Cristiano Ronaldo"],
+      correct_answer: "Messi"
+    },
+    {
+      question: "¿Cual es el pais mas grande del mundo?",
+      incorrect_answers: ["Brasil", "Canada", "Argentina"],
+      correct_answer: "Rusia"
     }
-  
-    // Obtener y mostrar la siguiente pregunta después de un breve retraso
-    setTimeout(() => {
-      obtenerPreguntaTrivia()
-        .then(pregunta => mostrarPregunta(pregunta))
-        .catch(error => console.error(error));
-      resultado.textContent = '';
-    }, 2000); // Espera 2 segundos antes de mostrar la siguiente pregunta
+  ];
+
+  // pregunta aleatoria 
+  return preguntas[Math.floor(Math.random() * preguntas.length)];
+}
+
+// mostrar una pregunta 
+function mostrarPregunta(pregunta) {
+  const preguntaTexto = document.getElementById('pregunta');
+  preguntaTexto.textContent = pregunta.question;
+
+  const respuestasContainer = document.getElementById('respuestas');
+  respuestasContainer.innerHTML = '';
+
+  const respuestas = [...pregunta.incorrect_answers, pregunta.correct_answer];
+  respuestas.sort(() => Math.random() - 0.5); // baraja las respuestas
+
+  respuestas.forEach(respuesta => {
+    const botonRespuesta = document.createElement('button');
+    botonRespuesta.textContent = respuesta;
+    botonRespuesta.addEventListener('click', () => comprobarRespuesta(respuesta, pregunta.correct_answer));
+    respuestasContainer.appendChild(botonRespuesta);
+  });
+}
+
+// comprobar si la respuesta es correcta
+function comprobarRespuesta(respuesta, respuestaCorrecta) {
+  if (respuesta === respuestaCorrecta) {
+    mostrarResultado('¡Respuesta correcta!');
+  } else {
+    mostrarResultado(`Respuesta incorrecta. La respuesta correcta era: ${respuestaCorrecta}`);
   }
-  
-  // Iniciar el juego obteniendo y mostrando la primera pregunta
-  obtenerPreguntaTrivia()
-    .then(pregunta => mostrarPregunta(pregunta))
-    .catch(error => console.error(error));
+}
+
+//  mostrar el resultado 
+function mostrarResultado(resultado) {
+  const resultadoElement = document.getElementById('resultado');
+  resultadoElement.textContent = resultado;
+
+  // obtener y mostrar la siguiente pregunta 
+  setTimeout(() => {
+      if (confirm('¿Quieres jugar otra vez?')) {
+          const siguientePregunta = obtenerPreguntaTrivia();
+          mostrarPregunta(siguientePregunta);
+          resultadoElement.textContent = '';
+      } else {
+         
+          console.log('Gracias por jugar. Hasta luego!');
+      }
+  }, 1000); // espera 1 segundo 
+}
+
+// iniciar el juego obteniendo mostrando la primera pregunta
+const primeraPregunta = obtenerPreguntaTrivia();
+mostrarPregunta(primeraPregunta);
